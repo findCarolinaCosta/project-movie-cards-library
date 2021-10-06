f9590af7b8661957eaf05ff345db13e8fecd6bae
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-10-06 20:55:32 -0300

    Updates

---
+++ precache-manifest.e208ec83e479af62e9237933b1804434.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7b006b8d5dd9bac7be5bbd7fdd949bb",
+    "url": "/project-movie-cards-library/index.html"
+  },
+  {
+    "revision": "7737ae1c0f7b929618b0",
+    "url": "/project-movie-cards-library/static/css/main.89f776bc.chunk.css"
+  },
+  {
+    "revision": "24e1e214416ac9376820",
+    "url": "/project-movie-cards-library/static/js/2.59a3b2c7.chunk.js"
+  },
+  {
+    "revision": "7737ae1c0f7b929618b0",
+    "url": "/project-movie-cards-library/static/js/main.39208b38.chunk.js"
+  },
+  {
+    "revision": "b5cd559347f125a80e22",
+    "url": "/project-movie-cards-library/static/js/runtime-main.136c6182.js"
+  }
+]);
